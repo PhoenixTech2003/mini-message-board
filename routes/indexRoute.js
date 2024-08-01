e3b921f7060b40f62e31dc5ec7de79716e037423
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-08-01 19:59:51 +0200

    Add the post request to the index router

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,5 +4,6 @@ const indexRouter = express.Router();
 
 indexRouter.get('/',indexControllers.renderIndexPage);
 indexRouter.get('/new', indexControllers.renderForm)
+indexRouter.post('/new', indexControllers.addMessage)
 
 module.exports = { indexRouter };
