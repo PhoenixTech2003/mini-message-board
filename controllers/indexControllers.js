1ced47c217e40fbfef348a3b50f01325654fa2b9
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-08-01 20:01:56 +0200

    Refactor the add message controlller

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -2,25 +2,30 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 const renderIndexPage = function rendersTheIndexPage(req, res) {
-  res.render("index", {messages: messages});
+  res.render("index", { messages: messages });
 };
 
 const renderForm = function rendersTheNewMessagePage(req, res) {
   res.render("form");
 };
 
-const addMessage = function thatAddsMessagesToTheMessageList(req, res){
-  res.send('hello from the post request')
-}
+const addMessage = function thatAddsMessagesToTheMessageList(req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+};
 
 module.exports = { renderIndexPage, renderForm, addMessage };
