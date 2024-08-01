2baaafe30d50c5d6f44e0e5ba885fa6dc43e562d
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-08-01 11:55:45 +0200

    Add the messages object

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,5 +1,18 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 const renderIndexPage = function rendersTheIndexPage(req, res) {
-  res.render("index");
+  res.render("index", {messages: messages});
 };
 
 module.exports = { renderIndexPage };
